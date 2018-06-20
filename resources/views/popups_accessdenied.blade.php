29d967b096e2a75a3e5043e902e0fab08420dfaa
trino <minesh@trinoweb.com>
2018-06-20 13:33:15 -0400

    Roy 1:33

--- resources/views/popups_accessdenied.blade.php
+++ resources/views/popups_accessdenied.blade.php
@@ -9,10 +9,14 @@
         font-weight: bolder;
         -webkit-text-stroke: 2px black;
         font-size: xx-large;
-
         background-image: linear-gradient(-45deg, white 25%, transparent 25%, transparent 50%, white 50%, white 75%, transparent 75%, transparent);
         background-size: 40px 40px;
+    }
 
+    .container-fluid{
+        padding-left: 0px;
+        padding-right: 0px;
+        border-bottom: solid 50px #DC3545;
     }
 </STYLE>
 <DIV CLASS="accessdenied bg-danger">ACCESS DENIED</DIV>
